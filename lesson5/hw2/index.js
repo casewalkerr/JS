0357613f2f5664caabdf85e646fe6bee791e8d05
Roman_Abakumov <case777walker@gmail.com>
2020-04-20 19:57:47 +0300

    1

--- lesson5/hw2/index.js
+++ lesson5/hw2/index.js
@@ -1,9 +1,11 @@
-function getPrimes(n){
-   nextPrime: for (let i = 2; i <= n; i++){
-        for(let j = 2; J < i; J++){
-        if(i % j == 0)  continue nextPrime;
-        
+function getPrimes(n) {
+   nextPrime: for (let i = 2; i <= n; i++) {
+     for (let j = 2; j < i; j++) {
+       if (i % j == 0) {
+          continue nextPrime;
       }
-    console.log(i);
- } 
-}
+     }
+ 
+     console.log(i);
+   }
+ }
