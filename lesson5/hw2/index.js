29254227e51f560a0b785ee365d6b2309bcd0b9a
Roman_Abakumov <case777walker@gmail.com>
2020-04-20 19:53:25 +0300

    1

--- lesson5/hw2/index.js
+++ lesson5/hw2/index.js
@@ -1,9 +1,10 @@
 function getPrimes(n){
    nextPrime: for (let i = 2; i <= n; i++){
-        for(let j = 2; J <= i; J++ ){
-        if(i % j === 0)
-        continue nextPrime;
-    }
+        for(let j = 2; J < i; J++){
+        if(i % j === 0){
+         continue nextPrime;
+        }
+      }
     console.log(i);
  } 
 }
