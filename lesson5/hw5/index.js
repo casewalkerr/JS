1f7ddc457895d467ad122e50b886013c999647d7
Roman_Abakumov <case777walker@gmail.com>
2020-04-20 15:49:18 +0300

    1

--- lesson5/hw5/index.js
+++ lesson5/hw5/index.js
@@ -2,7 +2,7 @@ function findDivCount(a, b, n){
    let sum = 0; 
    for(let i = a; i <= b; i++){
       if(i % n === 0 ){
-         sum += i; 
+         sum += 1; 
         }
        
      }return sum; 
